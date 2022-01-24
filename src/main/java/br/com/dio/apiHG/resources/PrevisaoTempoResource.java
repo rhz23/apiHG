daf55d4fb89c6f9bf7daeabc69dbdd7e1aa6b95d
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-01-24 17:56:28 -0300

    basic response with entity model

--- src/main/java/br/com/dio/apiHG/resources/PrevisaoTempoResource.java
+++ src/main/java/br/com/dio/apiHG/resources/PrevisaoTempoResource.java
@@ -1,6 +1,8 @@
 package br.com.dio.apiHG.resources;
 
+import br.com.dio.apiHG.models.PrevisaoTempoResponseModel;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrevisaoTempoResource {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/")
-    public String test(){
-        return "OK";
+    public ResponseEntity<PrevisaoTempoResponseModel> test(){
+        var cet =  PrevisaoTempoResponseModel.builder()
+                .nomeCidade("São Paulo")
+                .hora(16)
+                .temperatura(23)
+                .build();
+        return ResponseEntity.ok(cet);
     }
 }
